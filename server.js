ef5b61d798d425398a3c6043dbd115c9b7d01523
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-05-14 11:21:45 +0530

    update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express=require('express')
 const app=express()
 app.use(express.json())
 
+app.get('/',(req,res)=>{
+    res.send("Server is running")
+})
+
 app.post('/login',(req,res)=>{
     const{email,password}=req.body
 
